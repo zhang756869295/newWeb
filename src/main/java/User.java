185b591225ce8d0dcd95bd895e015cec385af7df
zhang <zhang@qq.com>
2017-12-20 10:05:54 +0800

    更加user类创建uname age pwd 属性

---
+++ src/main/java/User.java
@@ -0,0 +1,37 @@
+/**
+ * Created by 张 on 2017/12/20.
+ */
+
+/**
+ * 创建一个用户类
+ */
+public class User {
+    private String name;
+    private String ped;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPed() {
+        return ped;
+    }
+
+    public void setPed(String ped) {
+        this.ped = ped;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+}
